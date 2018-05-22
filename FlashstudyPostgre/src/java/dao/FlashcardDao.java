7b41fcc86abd61d41e2cf7c4efbc6a87a04a80b0
Igor Carvalho <costa.igor0108@gmail.com>
2018-05-22 16:29:33 -0400

    Alteração na classe dao

--- FlashstudyPostgre/src/java/dao/FlashcardDao.java
+++ FlashstudyPostgre/src/java/dao/FlashcardDao.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dao;
 
 import java.util.List;
@@ -11,10 +6,6 @@ import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
 
-/**
- *
- * @author Igor
- */
 public class FlashcardDao {
 
     private final Session sessao;
--- FlashstudyPostgre/src/java/dao/UsuarioDao.java
+++ FlashstudyPostgre/src/java/dao/UsuarioDao.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dao;
 
 import model.Usuario;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 
-/**
- *
- * @author Bruno
- */
 public class UsuarioDao {
 
     private final Session sessao;
